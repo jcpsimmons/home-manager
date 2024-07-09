d812a343917d778b9be8d58484540dc3892c7531
Josh <jsimmons@anduril.com>
2024-07-08 19:14:20 -0700

    last wezterm changes

--- programs/wezterm.nix
+++ programs/wezterm.nix
@@ -1,3 +1,32 @@
 { ... }:
 
-{ }
+{
+  programs.wezterm = {
+    enable = true;
+    # colorSchemes =
+    extraConfig = ''
+      local wezterm = require 'wezterm'
+      local config = {}
+
+      config.color_scheme = 'Homebrew (Gogh)'
+
+      config.font = wezterm.font 'BigBlueTermPlus Nerd Font'
+      config.font_size = 20
+      config.line_height = 1.07
+      config.text_background_opacity = 1.0
+
+      config.hide_tab_bar_if_only_one_tab = true
+
+      config.window_background_opacity = 1.0
+
+      config.initial_cols = 120
+      config.initial_rows = 40
+
+      config.window_decorations = "RESIZE"
+
+      config.text_blink_rate = 300
+
+      return config
+    '';
+  };
+}
